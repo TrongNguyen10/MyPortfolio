b84cf49ec94ecb0b65a24b1e75a74b981d7b5321
TrongNguyen10 <trungtrung1220@gmail.com>
2025-04-11 22:48:34 +0700

    add project details

---
+++ assets/js/projectInfo.js
@@ -0,0 +1,52 @@
+export const projectInfo = [
+	{
+		id: "app-1",
+		name: "Music Band",
+		tech: "HTML, CSS, Javascript",
+		date: "06/2024",
+		link: "https://trongnguyen10.github.io/Music-Band/",
+		img: ["app-1.png", "app-1-1.png", "app-1-2.png"],
+		description:
+			"Developed a responsive introduction website for a music band, showcasing the band’s profile, music, and events.",
+	},
+	{
+		id: "app-2",
+		name: "Music Player",
+		tech: "HTML, CSS, Javascript",
+		date: "09/2024",
+		link: "https://trongnguyen10.github.io/music-player/",
+		img: ["app-2.png", "app-2-1.png", "app-2-2.png"],
+		description:
+			"Created a mini music player website that allows users to play and pause music tracks, find songs and artists, add favorite song, enhancing user interaction and experience.",
+	},
+	{
+		id: "app-3",
+		name: "To do list",
+		tech: "HTML, CSS, Javascript",
+		date: "11/2024",
+		link: "https://trongnguyen10.github.io/todolist/",
+		img: ["app-3.png"],
+		description:
+			"Developed a task management application that allows users to add, edit, and delete notes, providing a user-friendly interface for productivity.",
+	},
+	{
+		id: "app-4",
+		name: "Online Shop",
+		tech: "HTML, CSS, Typescript, ReactJS",
+		date: "10/2024",
+		link: "https://ndtfashion.netlify.app/",
+		img: ["app-4.png", "app-4-1.png", "app-4-2.png"],
+		description:
+			"Built an online shopping platform enabling users to browse, manage products, and make purchases. Focused on creating a seamless user experience and integrating a RESTful API for data management.",
+	},
+	{
+		id: "app-5",
+		name: "Tiktok UI",
+		tech: "HTML, CSS, Javascript, ReactJS",
+		date: "02/2025",
+		link: "https://ndt-tiktokui.netlify.app/",
+		img: ["app-5.png", "app-5-1.png", "app-5-2.png"],
+		description:
+			"Creating a social media application similar to TikTok, where users can upload videos and follow other users. Focused on user engagement and performance optimization.",
+	},
+]
